6f2ecf3f4cf423031898f09c1cc5b6d4ccb4b5ce
Jorge_Charry <jacharrym@unal.edu.co>
2016-02-10 14:49:07 -0500

    Reorganize the code

--- core.py
+++ core.py
@@ -1,21 +1,27 @@
-# Module wick
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import signal
+from itertools import *
+import itertools 
 import copy
+import sys
 
-# Global variables
-typeOfCombination = {
-0:"Zero",
-1:"Singles",
-2:"Doubles",
-3:"Triples, not tested!",
-4:"Quadruples, not tested!",
-}
+## =====================
+## Python program to solve the Wick's theorem for a chain of operators in latex format. 
+## =====================
+
+## =====================
+## User options
+## =====================
 
 fermions = True
 bosons = False
 printZeroValues = False
-occupiedIndexes = ("i","j","k","l","m","n","o","p","q","r","s")
+occupiedIndexes = ("i","j","k","l","m","n","o","p","q","r","s","t","u")
 virtualIndexes = ("a","b","c","d","e","f","g","h")
+
+#Generate the terms
 
 ## subOperators
 class subOperators(object):
@@ -25,45 +31,10 @@ class subOperators(object):
 		self.scalar = scalar
 
 
-## Convert a list to a string
-def longformat ( vector ) :
-	string = ""
-	for i in vector :
-		string = string + i
-	string = string[0:]
-	return string
 
-def transformToFermiSpace ( vector ) :
-	auxVector = list()
-	for i in range(0,len(vector)) :
-		auxIndex = index(vector[i])		
-		if lower(auxIndex) in occupiedIndexes :
-			if dagger(vector[i]) == 1 :
-				auxVector.append ("b_"+"{"+auxIndex+"}")
-			elif dagger(vector[i]) == 0 :
-				auxVector.append ("b_"+"{"+auxIndex+"}^{\dagger}")
-		elif lower(auxIndex) in virtualIndexes :
-			if dagger(vector[i]) == 1 :
-				auxVector.append ("b_"+"{"+auxIndex+"}^{\dagger}")
-			elif dagger(vector[i]) == 0 :
-				auxVector.append ("b_"+"{"+auxIndex+"}")
-
-	return auxVector
-
-## Return the subindex of the operator
-def index ( operator ) :
-	auxindex = operator.split("_")[1]
-	initial = auxindex.find("{")
-	final = auxindex.find("}")
-	return auxindex[initial+1:final]
-
-def lower(String):
-	String = String.lower()
-	return String
-
-def upper(String):
-	String = String.upper()
-	return String
+## =====================
+## General Funtions 
+## =====================
 
 ## Creation operator = 1
 ## Annhiliation operator = 0
@@ -72,6 +43,47 @@ def dagger ( operator ) :
 		return 1
 	else :
 		return 0
+
+def searchSuperOperator ( superOperator ) :
+	if "\hat" in superOperator: 
+		return True
+	else :
+		return False
+
+## Return the subindex of the operator
+def index ( operator ) :
+	auxindex = operator.split("_")[1]
+	initial = auxindex.find("{")
+	final = auxindex.find("}")
+	return auxindex[initial+1:final]
+
+## Return the subindex of the operator
+def checkDoubleIndex ( operator ) :
+	auxindex = operator.split("_")[1]
+	initial = auxindex.find("{")
+	final = auxindex.find("}")
+
+	index1 = auxindex[initial+1]
+	index2 = auxindex[final-1]
+
+	if index1.isupper() and index2.isupper() :
+		return True
+	elif  index1.islower() and index2.islower() :
+		return True
+	else :
+		return False
+
+## Evaluate if the vector is sorted in reverse form
+def evaluateOrder ( vector ):
+	
+	sortedVector = sorted(vector, reverse=True)
+
+	isSorted = True
+	for i in range(0, len(vector)) :
+		if vector[i] != sortedVector[i] :
+			isSorted = False
+
+	return isSorted
 
 ## Return the chain of operators in its normal order
 def normalOrder ( vector, auxSign ) :
@@ -107,24 +119,6 @@ def normalOrder ( vector, auxSign ) :
 
 	return outputVector, auxSign
 
-## Evaluate if the vector is sorted in reverse form
-def evaluateOrder ( vector ):
-	
-	sortedVector = sorted(vector, reverse=True)
-
-	isSorted = True
-	for i in range(0, len(vector)) :
-		if vector[i] != sortedVector[i] :
-			isSorted = False
-
-	return isSorted
-
-## Chain of operator object
-class operatorchain(object):
-	def __init__(self,sign,chain):
-		self.sign = sign
-		self.chain = chain
-
 ## Commutator object
 class commutator(object):
 	def __init__(self,sign,string):
@@ -155,21 +149,109 @@ def evaluateContraction ( operator1, operator2 ) :
 def removeOperator ( vector, index ) :
 	del vector[index]
 
-## Return the subindex of the operator
-def checkDoubleIndex ( operator ) :
-	auxindex = operator.split("_")[1]
-	initial = auxindex.find("{")
-	final = auxindex.find("}")
+## Return the sign "+" or "-" just for printing
+def symbolOfSign (sign) :
+	if sign == 1:
+		return "+"
+	elif sign == -1:
+		return "-" 
+	elif sign == 0:
+		return ""
 
-	index1 = auxindex[initial+1]
-	index2 = auxindex[final-1]
+## Convert a list to a string
+def longformat ( vector ) :
+	string = ""
+	for i in vector :
+		string = string + i
+	string = string[0:]
+	return string
 
-	if index1.isupper() and index2.isupper() :
-		return True
-	elif  index1.islower() and index2.islower() :
-		return True
-	else :
-		return False
+def lower(String):
+	String = String.lower()
+	return String
+
+def upper(String):
+	String = String.upper()
+	return String
+
+def transformToFermiSpace ( vector ) :
+	auxVector = list()
+	for i in range(0,len(vector)) :
+		auxIndex = index(vector[i])		
+		if lower(auxIndex) in occupiedIndexes :
+			if dagger(vector[i]) == 1 :
+				auxVector.append ("b_"+"{"+auxIndex+"}")
+			elif dagger(vector[i]) == 0 :
+				auxVector.append ("b_"+"{"+auxIndex+"}^{\dagger}")
+		elif lower(auxIndex) in virtualIndexes :
+			if dagger(vector[i]) == 1 :
+				auxVector.append ("b_"+"{"+auxIndex+"}^{\dagger}")
+			elif dagger(vector[i]) == 0 :
+				auxVector.append ("b_"+"{"+auxIndex+"}")
+
+	return auxVector
+
+## =====================
+## Wick's theorem
+## =====================
+
+V0 = subOperators (+1,["a_{p}^{\dagger}", "a_{Q}^{\dagger}","\hat{H}","a_{r}","a_{S}" ], "" )
+# wX \hat{A} yZ
+# = [X^\dagger w^\dagger , \hat{A} y Z ]_+
+# = X^\dagger w^\dagger \hat{A} y Z + \hat{A} y Z  X^\dagger w^\dagger
+# = X^\dagger w^\dagger y Z A (1) - X^\dagger w^\dagger A y Z (2)  + 
+# y Z A  X^\dagger w^\dagger (3) - A y Z  X^\dagger w^\dagger (4)
+
+V1 = subOperators (+1,[V0.string[1], V0.string[0], V0.string[3], V0.string[4], V0.string[2]], "" )
+if "{H}" in V1.string[4] :
+	V1.scalar = "E"
+	del V1.string[4]
+V2 = subOperators (-1,[V0.string[1], V0.string[0], V0.string[2], V0.string[3], V0.string[4]], "" )
+if "{H}" in V2.string[2] :
+	V2.scalar = "E" + "-\epsilon_{" + index(V2.string[3]) + "}" + "-\epsilon_{" + index(V2.string[4])
+	del V2.string[2]
+V3 = subOperators (+1,[V0.string[3], V0.string[4], V0.string[2], V0.string[1], V0.string[0]], "" )
+if "{H}" in V3.string[2] :
+	V3.scalar = "E" + "+\epsilon_{" + index(V3.string[0]) + "}" + "+\epsilon_{" + index(V3.string[1])
+	del V3.string[2]
+V4 = subOperators (-1,[V0.string[2], V0.string[3], V0.string[4], V0.string[1], V0.string[0]], "" )
+if "{H}" in V4.string[0] :
+	V4.scalar = "E"
+	del V4.string[0]
+
+allV = [V1,V2,V3,V4]
+#V1.string.append("a_{t}")
+#V1.string.append("a_{u}")
+#allV = [V1]
+	
+
+## Please do not set both variables True or false at the same time...
+if ( fermions == True and bosons == True ) or ( fermions == False and bosons == False ) :
+	sys.exit("Please use fermions or bosons...")
+
+## =====================
+# Method 2
+## =====================
+print "="*10
+print "== Method 2"
+print "="*10
+
+#Variables
+
+## 
+typeOfCombination = {
+0:"Zero",
+1:"Singles",
+2:"Doubles",
+3:"Triples, not tested!",
+4:"Quadruples, not tested!",
+}
+
+## Chain of operator object
+class operatorchain(object):
+	def __init__(self,sign,chain):
+		self.sign = sign
+		self.chain = chain
 
 ## Generate all the posible combinations of the Wick's theorem with a recursive method: singles, doubles, triplets...
 def generateCombinations ( matrixOfCombinations, ncombination, ntype, vector, outputValue ):
@@ -242,11 +324,13 @@ def generateCombinations ( matrixOfCombinations, ncombination, ntype, vector, ou
 	return matrixOfCombinations, ncombination, ntype, outputV, outputValue
 
 
+## Generate all the combinations
 
-def wick (Vi) :
+for Vi in allV :
 
-
+	print "="*10
 	print "== Initial"
+	print "="*10
 	print "\t",longformat(Vi.string)
 
 	print "== Fermi vacuum"
@@ -264,28 +348,21 @@ def wick (Vi) :
 	for i in range(0, totalCombinations+1):
 		matrixOfCombinations.append( list() )
 
-	# add the zero term to the matrix
 	matrixOfCombinations[0].append ( operatorchain ( sign,longformat(NV) ) )
 
 	auxint = 0
 	matrixOfCombinations, auxint, auxint, outputVector, outputValue = generateCombinations ( \
 		matrixOfCombinations, totalCombinations, 0, Vi.string, operatorchain (1,""))
 
-	## Printing the values
-	print "original"
-	for i in range(0, totalCombinations+1):
-		print typeOfCombination[i]
-		for j in range(0,len(matrixOfCombinations[i])):
-			print "\t",matrixOfCombinations[i][j].chain
-
 	## Checking the zero terms
 	auxMatrixOfCombinations = copy.deepcopy(matrixOfCombinations)
 	for i in range(0, totalCombinations+1):
 		# Select only the non-zero terms after operates over the HF wavefunction in the fermi vacuum
 		if len (matrixOfCombinations[i]) > 0 :
+			print typeOfCombination[i]
 ## 		Here we need to check the doubles indexes (iJ) 
 			for j in range(0,len(matrixOfCombinations[i])):
-				print matrixOfCombinations[i][j].chain[-1]
+				print "\t",matrixOfCombinations[i][j].sign, matrixOfCombinations[i][j].chain
 				# It will remove all terms with an anhiliation operator to the left
 				if dagger(matrixOfCombinations[i][j].chain[-1]) == 0 and \
 				"\delta_{" not in (matrixOfCombinations[i][j].chain[-1]) : 
@@ -297,8 +374,6 @@ def wick (Vi) :
 						auxMatrixOfCombinations[i][j] = None
 						break
 
-	print "remove zeros"
-
 	# Clean all zero terms for each type of combinations
 	for i in range(len(auxMatrixOfCombinations)-1,-1, -1):
 		auxMatrixOfCombinations[i] = filter (None,  auxMatrixOfCombinations[i] )
@@ -309,23 +384,10 @@ def wick (Vi) :
 		for j in range(0, len(auxMatrixOfCombinations[i])):
 			print auxMatrixOfCombinations[i][j].chain
 
+#	print auxMatrixOfCombinations[1][0].chain
 
+	del matrixOfCombinations 
 
-## 
-
-
-################################################
-################################################
-################################################
-
-#V0 = ["a_{p}^{\dagger}", "a_{q}^{\dagger}","a_{r}","a_{s}" ]  
-V0 = subOperators (+1,["a_{p}^{\dagger}", "a_{Q}^{\dagger}","\hat{H}","a_{r}","a_{S}" ], "" )
-V1 = subOperators (+1,[V0.string[1], V0.string[0], V0.string[3], V0.string[4], V0.string[2]], "" )
-if "{H}" in V1.string[4] :
-	V1.scalar = "E"
-	del V1.string[4]
-
-# call wick
-wick (V1)
-
-
+## =====================
+## END PROGRAM
+## ====================
